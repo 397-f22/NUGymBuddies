f458bd10d0d3c05cd1bf1f1de323e8d3375d4e46
Chris Naing <clnaing23@gmail.com>
2022-10-27 16:08:14 -0500

    datepicker 2

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,24 +1,27 @@
 import * as React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-export default function StaticDatePickerLandscape() {
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-07'));
+export default function ResponsiveDatePickers() {  
+  const [value, setValue] = React.useState(dayjs(new Date()));
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker
-        orientation="landscape"
-        openTo="day"
+      <MobileDatePicker
+        label="Gym Days"
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
-        }}
+         }}
         renderInput={(params) => <TextField {...params} />}
       />
     </LocalizationProvider>
   );
 }
+
