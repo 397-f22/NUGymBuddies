09be7716009876cb2f182e5fc96e85e952f8c146
zhenming wang <zhenmingwang@zhenmingdembp.ads.northwestern.edu>
2022-10-30 15:32:10 -0500

    get date

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,40 @@ import NavBar from './components/NavBar';
 import GymCard from './components/GymCard';
 import Datepicker from './components/Datepicker';
 import Gyms from './Gyms';
+import {useState,useEffect} from 'react';  
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="App-nav">
-        <NavBar />
-        <div className="text">
-          <h3>Fancy a workout?</h3>
-        </div>
-        <div className="date-picker">
-        <Datepicker/>
-        </div>
-        {Gyms.map(gym => (
-          <div className='card-container'>
-          <GymCard name={gym.name} location={gym.location} popular_times={gym.popular_times} max_cap={gym.max_cap}/>
+  const [day,setDay] = useState(null);
+  useEffect(()=>{
+    const start = Date.now();
+    const today = new Date(start);
+    // console.log();
+    setDay(today.toDateString().slice(0,3))
+
+  },[])
+  if(day!=null){
+    return (
+      <div className="App">
+        <div className="App-nav">
+          <NavBar />
+          <div className="text">
+            <h3>Fancy a workout?</h3>
           </div>
-        ))}
+          <div className="date-picker">
+          <Datepicker setDay={setDay}/>
+          </div>
+          {Gyms.map(gym => (
+            <div className='card-container'>
+            <GymCard name={gym.name} location={gym.location} popular_times={gym.popular_times} date={day} max_cap={gym.max_cap}/>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  
+
+
+  }
 };
 
 export default App;
--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -4,10 +4,17 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-
-export default function ResponsiveDatePickers() {  
+import { useEffect } from 'react';
+export default function ResponsiveDatePickers({setDay}) {  
   const [value, setValue] = React.useState(dayjs(new Date()));
+  // useEffect(()=>{
 
+  //   //get date
+  //   let day = value["$d"]
+  //   // console.log(day);
+  //   day = day.toString().slice(0,3);
+  //   console.log(day);
+  // },[value])
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <MobileDatePicker
@@ -15,6 +22,7 @@ export default function ResponsiveDatePickers() {
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
+          setDay(newValue["$d"].toString().slice(0,3));
          }}
         renderInput={(params) => <TextField {...params} />}
       />
