6e6ad2ab43be58ed9e4799c22980e09ad0fb9378
jazzgobbo <jazzgobbo@gmail.com>
2022-11-03 17:15:15 -0500

    in process of changing chart color

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -6,11 +6,18 @@ const Chart = ({popular_times, date, time})=>{
   useEffect(()=>{
     console.log("chart",date);
   },[date])
-    //console.log(time);
+    console.log(time);
     const x_data = ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p','8p','9p','10p','11p'];
    
     const y_data_before = popular_times[date].filter((x,i)=> i>=6);
     const y_data = y_data_before.map((x,i)=> parseInt(x.replace("%","")));
+    const threshold = 25;
+    console.log("y_data", y_data);
+    const y_data_green = y_data.filter((x,i)=> (i in time && x < threshold));
+    const y_data_red = y_data.filter((x,i)=> (i in time && x >= threshold));
+    const y_data_blue = y_data.filter((x,i)=> !(i in time));
+    console.log("green", y_data_green, "red", y_data_red, "blue", y_data_blue );
+   
     const option = {
       
         xAxis: {
@@ -22,9 +29,26 @@ const Chart = ({popular_times, date, time})=>{
         },
         series: [
           {
-            data: y_data,
+            data: y_data_blue,
             type: 'bar',
-          }
+            itemStyle: {
+              color: "blue"
+            }
+          },
+          {
+            data: y_data_green,
+            type: 'bar',
+            itemStyle: {
+              color: "green"
+            }
+          },
+          {
+            data: y_data_red,
+            type: 'bar',
+            itemStyle: {
+              color: "red"
+            }
+          },
         ],
       };
     
