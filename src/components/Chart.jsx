23326e0ce9cadaeac1645222dc4aea1abd2e56cb
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-11-03 19:26:59 -0500

    Attempt to add legent to chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,19 +1,18 @@
-import ReactECharts from 'echarts-for-react';
+import ReactEcharts from "echarts-for-react";
 import { useEffect } from 'react';
 import "./Chart.css";
+import { blue, green, red } from '@mui/material/colors';
 
 const Chart = ({popular_times, date, time})=>{
   useEffect(()=>{
     // console.log("chart",date);
   },[date])
-    console.log(time);
     const x_data = ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p','8p','9p','10p'];
     const x_data_form_time = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
    
     const y_data_before = popular_times[date].filter((x,i)=> i>=6);
     const y_data = y_data_before.map((x,i)=> parseInt(x.replace("%","")));
     const threshold = 25;
-    console.log("y_data", y_data);
     const y_data_green = x_data_form_time.map((x)=> {
       if (time.includes(x.toString()) && y_data[x-6] < threshold) {
         return y_data[x-6];
@@ -36,9 +35,21 @@ const Chart = ({popular_times, date, time})=>{
       }
     });
     console.log("green", y_data_green, "red", y_data_red, "blue", y_data_blue );
-   
-    const option = {
-      
+
+    const legendData = [["Unselected Time", "blue"], ["Selected Time Over Capacity", red], ["Selected Time Under Capacity", green]];
+
+    const dataNames = legendData.map(x=>x[0]);
+
+    let option = {
+      legend: {
+        icon: "circle",
+        x: "center",
+        y: "50px",
+        data: dataNames,
+        textStyle: {
+          color: "#fff"
+        }
+      },
         xAxis: {
           type: 'category',
           data: x_data
@@ -75,8 +86,14 @@ const Chart = ({popular_times, date, time})=>{
       //   height: '8.1vh',
       //   width: '100%',
       //   padding: "3%",}}
+      
+      // The usage of ReactEChartsCore are same with above.
+      return <ReactEcharts option={option} />
 
-      return <ReactECharts option={option} />;
+      // return <ReactEChartsCore
+      //           echarts={echarts}
+      //           option={option}
+      //         />
 
 
 }
