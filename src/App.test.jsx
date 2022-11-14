e2a9d6fd7bef39a79dc1e253fa70af3aca806bb2
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-11-13 20:42:02 -0600

    Install canvas as dependency and remove cyprus job

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,12 @@
-// import {describe, expect, test} from 'vitest';
-// import {fireEvent, render, screen} from '@testing-library/react';
-// import App from './App';
+import {describe, expect, test} from 'vitest';
+import {fireEvent, render, screen} from '@testing-library/react';
+import App from './App';
 
-// describe('render tests', () => {
+describe('render tests', () => {
     
-//   test("App renders successfully", () => {
-//     render(<App />);
-//     expect(screen.getByText('NU')).toBeDefined();
-//   });
+  test("App renders successfully", () => {
+    render(<App />);
+    expect(screen.getByText('NU')).toBeDefined();
+  });
 
-// });
+});
