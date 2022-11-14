c4070fce2a9158c105c08659b2c5765aaa7d24e4
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-11-13 21:14:50 -0600

    Updated token

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,7 +4,7 @@ import App from './App';
 
 describe('render tests', () => {
     
-  test("App renders successfully", () => {
+  test("App renders successfully with text NU", () => {
     render(<App />);
     expect(screen.getByText('NU')).toBeDefined();
   });
